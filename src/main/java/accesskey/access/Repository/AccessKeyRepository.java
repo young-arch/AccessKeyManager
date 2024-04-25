a2d90d7dd8b2ad528e463d190dd51e6a06e2d5cf
MawuleG <gabriel.sakyi@ucc.edu.gh>
2024-04-25 02:55:25 +0100

    Added Repository classes implementation for project

---
+++ src/main/java/accesskey/access/Repository/AccessKeyRepository.java
@@ -0,0 +1,19 @@
+package accesskey.access.Repository;
+
+import accesskey.access.Entity.AccessKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AccessKeyRepository extends JpaRepository<AccessKey, Integer> {
+    //This finds all access keys for a specific user
+    List<AccessKey> findByUserId(Integer userId);
+
+    //This finds all the active access key for a specific user
+    AccessKey findByUserIdAndStatus(Integer userId, String status);
+
+    //Checks if there is an active key for a specific key
+    boolean existsByUserIdAndStatus(Integer userId, String status);
+}
---
+++ src/main/java/accesskey/access/Repository/UserRepository.java
@@ -0,0 +1,15 @@
+package accesskey.access.Repository;
+
+import accesskey.access.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+
+}
