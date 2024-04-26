2b2528055b9f6548f64a1799392819092a0be5f9
young-arch <mawulegabriel@gmail.com>
2024-04-26 02:54:11 +0100

    Added the UserService class

--- src/main/java/accesskey/access/Service/UserService.java
+++ src/main/java/accesskey/access/Service/UserService.java
@@ -1,5 +1,6 @@
 package accesskey.access.Service;
 
+import accesskey.access.Entity.User;
 import accesskey.access.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -8,13 +9,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService{
 
-    private UserRepository userRepository;
-    private PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
     public UserService(UserRepository userRepository,PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    //Create a new user
+    public User createUser(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    //Find a user by email
+    public User findUserByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+
+    //Update a user's password
+    public void updatePassword(String email, String newPassword){
+        String encodedPassword = passwordEncoder.encode(newPassword);
+        userRepository.updatePasswordByEmail(email, encodedPassword);
     }
 
 }
