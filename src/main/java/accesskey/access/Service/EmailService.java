17f80aa74037b32cbdca343f36077fdef5dc5462
young-arch <mawulegabriel@gmail.com>
2024-05-07 02:49:47 +0100

    Added the EmailService class

---
+++ src/main/java/accesskey/access/Service/EmailService.java
@@ -0,0 +1,19 @@
+package accesskey.access.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender mailSender){
+        this.mailSender = mailSender;
+    }
+
+
+
+}
