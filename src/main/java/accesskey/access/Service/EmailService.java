8b3166f1090922ea5b53d8daef32d4965bd4ceed
young-arch <mawulegabriel@gmail.com>
2024-05-07 02:55:25 +0100

    Added a method to send email with given details

--- src/main/java/accesskey/access/Service/EmailService.java
+++ src/main/java/accesskey/access/Service/EmailService.java
@@ -1,6 +1,7 @@
 package accesskey.access.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,14 @@ public class EmailService {
     @Autowired
     public EmailService(JavaMailSender mailSender){
         this.mailSender = mailSender;
+    }
+
+    public void sendEmail(String to, String subject, String text){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
     }
 
 
