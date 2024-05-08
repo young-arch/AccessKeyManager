d11ce18cd486d9faf239961457cb74e73d25253e
young-arch <mawulegabriel@gmail.com>
2024-05-08 20:58:49 +0100

    Added the reset password method

--- src/main/java/accesskey/access/Controller/UserController.java
+++ src/main/java/accesskey/access/Controller/UserController.java
@@ -55,7 +55,8 @@ public class UserController{
 
     //Password Reset
     public ResponseEntity<Void> resetPassword(@RequestParam("token") String token, @RequestParam("password") String newPassword){
-        userService.resetPassword()
+        userService.resetPassword(token, newPassword);
+        return ResponseEntity.noContent().build();
     }
 
 
