ea60a6a29ecc0a08234da7a101aee962cd1413fb
young-arch <mawulegabriel@gmail.com>
2024-05-11 20:42:43 +0100

    Resolved errors caused by update method in the user classes

--- src/main/java/accesskey/access/Repository/UserRepository.java
+++ src/main/java/accesskey/access/Repository/UserRepository.java
@@ -2,7 +2,11 @@ package accesskey.access.Repository;
 
 import accesskey.access.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -11,7 +15,10 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     boolean existsByEmail(String email);
 
-    void updatePasswordByEmail(String email, String newPassword);
+    @Modifying
+    @Transactional
+    @Query("UPDATE User u SET u.password = :newPassword WHERE u.email = :email")
+    void updatePasswordByEmail(@Param("email") String email, @Param("newPassword") String newPassword);
 
     User findByResetToken(String token);
 
