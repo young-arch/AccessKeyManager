af0b3feb81b9ed5a497bcd6bab696071cf12b619
young-arch <mawulegabriel@gmail.com>
2024-06-06 13:59:36 +0100

    All tests passed without any issues

--- src/test/java/accesskey/access/Service/UserServiceTest.java
+++ src/test/java/accesskey/access/Service/UserServiceTest.java
@@ -20,7 +20,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 class UserServiceTest {
-
+    //All tests passed with no issues
     @Mock
     private UserRepository userRepository;
 
