a16a8339f0168fb150815cc9f8a786cb7d0bc747
young-arch <mawulegabriel@gmail.com>
2024-06-06 08:59:52 +0100

    Enhanced the Frontend

--- src/main/java/accesskey/access/Controller/UserWeb.java
+++ src/main/java/accesskey/access/Controller/UserWeb.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -22,16 +25,16 @@ public class UserWeb {
     private UserService userService;
 
     @GetMapping()
-    public String userKeys(Authentication authentication, Model model){
+    public String userKeys(Authentication authentication, Model model) {
         String email = authentication.getName();
-        model.addAttribute("favicon", email);
+        model.addAttribute("email", email);
         List<KeyDetails> myKeys = accessKeyService.getAllAccessKeysByEmail(email);
-        model.addAttribute("accessKeys", myKeys); // Note the attribute name change
+        model.addAttribute("accessKeys", myKeys);
         return "userPanel";
     }
 
     @PostMapping("/create")
-    public String createKey(@RequestParam String customKeyName, Authentication authentication){
+    public String createKey(@RequestParam String customKeyName, Authentication authentication) {
         accessKeyService.createAccessKeyWithCustomName(customKeyName, userService.findUserByEmail(authentication.getName()).getId());
         return "redirect:/user";
     }
